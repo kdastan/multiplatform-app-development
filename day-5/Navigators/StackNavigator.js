0b2b58a7f7260c04dfc0fcfcc91dc4ce9e636397
kumardastan <kumardastan@gmail.com>
2019-02-07 18:30:30 +0600

    feat(day-5): upload day-5 source code

---
+++ day-5/Navigators/StackNavigator.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation'; // Version can be specified in package.json
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => {
+            this.props.navigation.navigate("Details")
+          }}
+        />
+      </View>
+    );
+  }  
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go Back"
+          onPress={() => {
+            this.props.navigation.goBack()
+            
+          }}
+        />
+      </View>
+    );
+  }  
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  Details: {
+    screen: DetailsScreen,
+  },
+}, {
+    initialRouteName: 'Home',
+});
+
+export default createAppContainer(AppNavigator);
---
+++ day-5/unix-commands-and-npm.md
@@ -0,0 +1,49 @@
+
+# Basic Unix commands
+
+To feel confident while working on our course with the terminal, you need to know the basic UNIX commands to work in the terminal.
+
+### Properties
+
+| Command    | Example | Description |
+| :------ | :-----: | :--------: |
+| ls |  ls  |    Lists files in current directory    | 
+| cd   |  cd file-name    |   Change directory to file-name    | 
+| cd   |  cd ..   |   Move back one directory     | 
+| mkdir    |  mkdir new-file  |   Make a directory called new-file    |
+| rm |   rm file-name   |   Remove file file-name    |
+
+# NPM
+
+### Downloading and installing
+
+- Installing an package:
+
+```bash    
+npm install <package_name>
+```
+
+Example:
+
+```bash
+npm install react-navigation
+```
+
+- Uninstalling an package:
+
+```bash    
+npm uninstall <package_name>
+```
+
+Example:
+
+```bash
+npm uninstall react-navigation
+```
+
+# Useful links for additional information
+
+- [Downloading and installing nom packages](https://docs.npmjs.com/downloading-and-installing-packages-locally)
+- [Basic UNIX Commands](https://www.tjhsst.edu/~dhyatt/superap/unixcmd.html)
+
+
